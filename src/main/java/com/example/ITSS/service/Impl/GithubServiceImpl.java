417a74b5d026307d2b03e7124206d6c2f6efff01
dungnguyentri04 <nguyentridungmk@gmail.com>
2025-06-13 00:10:32 +0700

    github

--- src/main/java/com/example/ITSS/service/Impl/GithubServiceImpl.java
+++ src/main/java/com/example/ITSS/service/Impl/GithubServiceImpl.java
@@ -1,9 +1,17 @@
 package com.example.ITSS.service.Impl;
 
+import com.example.ITSS.dto.responseDto.GitContributionResponseDto;
 import com.example.ITSS.exception.NotFoundException;
 import com.example.ITSS.models.Class;
+import com.example.ITSS.models.GitContribution;
+import com.example.ITSS.models.Project;
+import com.example.ITSS.models.ProjectClassMember;
 import com.example.ITSS.repositories.ClassRepository;
+import com.example.ITSS.repositories.GitContributionRepository;
+import com.example.ITSS.repositories.ProjectClassMemberRepository;
+import com.example.ITSS.repositories.ProjectRepository;
 import com.example.ITSS.service.GithubService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -13,23 +21,62 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class GithubServiceImpl implements GithubService {
     @Autowired
     private ClassRepository classRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private GitContributionRepository gitContributionRepository;
+
+    @Autowired
+    private ProjectClassMemberRepository projectClassMemberRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
-    public List<Map<String, Object>> getGithubData(Long projectId) {
-        Class aClass = classRepository.findById(projectId).orElseThrow(
+    public List<GitContributionResponseDto> getGithubData(Long projectId) {
+        Project project = projectRepository.findById(projectId).orElseThrow(
                 () -> new NotFoundException("Project not found")
         );
-        String githubLink = aClass.getGithubLink();
-        String token = aClass.getToken();
+        List<ProjectClassMember> projectClassMembers = projectClassMemberRepository.findByProjectId(projectId);
+        Map<String, String> githubNameToUsernameMap = projectClassMembers.stream()
+                .filter(member -> member.getNameGithub() != null && member.getUsername() != null)
+                .collect(Collectors.toMap(
+                        ProjectClassMember::getNameGithub,
+                        ProjectClassMember::getUsername
+                ));
+
+        List<GitContributionResponseDto> gitContributionResponseDtos = gitContributionRepository.findByProjectIdAndGithubLink(projectId, project.getGithubLink())
+                .stream()
+                .map(gitContribution -> {
+                    GitContributionResponseDto gitContributionResponseDto = modelMapper.map(gitContribution, GitContributionResponseDto.class);
+                    String username = githubNameToUsernameMap.get(gitContribution.getNameGithub());
+                    if (username != null) {
+                        gitContributionResponseDto.setUsername(username);
+                    }
+                    return gitContributionResponseDto;
+                }).toList();
+
+        return gitContributionResponseDtos;
+    }
+
+    @Override
+    public String updateGithubData(Long projectId) {
+        Project project = projectRepository.findById(projectId).orElseThrow(
+                () -> new NotFoundException("Project not found")
+        );
+        String githubLink = project.getGithubLink();
+        String token = project.getToken();
         if (githubLink == null || token == null) {
             throw new NotFoundException("Can't get github data");
         }
@@ -38,76 +85,74 @@ public class GithubServiceImpl implements GithubService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "token " + token);
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        Set<String> existingShaCommit = gitContributionRepository.findCommitHashesByProjectId(projectId);
 
-        //get contributors
-        List<String> contributors = getContributors(githubLink, entity, restTemplate);
-
-        //get Data commit
-        for (String contributor : contributors) {
-            Map<String, Object> contributorCommit = new HashMap<>();
-            contributorCommit.put("contributor", contributor);
-            List<Map<String, Object>> listCommitsData = new ArrayList<>();
-            List<String> listShaCommit = getListShaCommit(githubLink, entity, restTemplate, contributor);
-            for (String sha : listShaCommit) {
-                Map<String, Object> commitData = lisCommitData(githubLink, entity, restTemplate, sha);
-                listCommitsData.add(commitData);
-            }
-            contributorCommit.put("commits", listCommitsData);
-            result.add(contributorCommit);
+        //get list sha commit
+        List<String> listShaCommit = new ArrayList<>();
+        try {
+            listShaCommit = getListShaCommit(githubLink, entity, restTemplate, existingShaCommit);
         }
-        return result;
+        catch (Exception e) {
+            return "Can't get github data";
+        }
+        for (String shaCommit : listShaCommit) {
+            try {
+                GitContribution gitContribution = getCommitData(githubLink, entity, restTemplate, shaCommit,  projectId);
+                gitContributionRepository.save(gitContribution);
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        return "Update github data successfully";
     }
 
-    private List<String> getContributors(String githubLink, HttpEntity<String> entity, RestTemplate restTemplate) {
-        String pullsUrl = githubLink + "/contributors";
-        List<String> result = null;
-        ResponseEntity<List<Map<String, Object>>> pullsResponse = restTemplate.exchange(
-                pullsUrl,
-                HttpMethod.GET,
-                entity,
-                new ParameterizedTypeReference<List<Map<String, Object>>>() {}
-        );
-        return pullsResponse.getBody().stream().map(
-                response -> {
-                    String login = (String) response.get("login");
-                    return login;
-                }
-        ).toList();
-    }
-
-    private Map<String, Object> lisCommitData(String githubLink, HttpEntity<String> entity, RestTemplate restTemplate, String sha) {
-        String pullsUrl = githubLink + "/commits/" + sha;
+    private GitContribution getCommitData(String githubLink, HttpEntity<String> entity, RestTemplate restTemplate, String shaCommit, Long projectId) {
+        String pullsUrl = String.format("https://api.github.com/repos/%s/commits/%s", githubLink, shaCommit);
         ResponseEntity<Map<String, Object>> pullsResponse = restTemplate.exchange(
                 pullsUrl,
                 HttpMethod.GET,
                 entity,
-                new ParameterizedTypeReference<Map<String, Object>>() {}); // <Map>
-        Map<String, Object> dataResponse = pullsResponse.getBody();
-        Map<String, Object> commitData = (Map<String, Object>) dataResponse.get("commit"); // <Map>
-        Map<String, Object> stats = (Map<String, Object>) commitData.get("stats"); // <Map>
-        dataResponse.put("message", commitData.get("message"));
-        dataResponse.put("comment_count", commitData.get("comment_count"));
-        dataResponse.put("line_change", stats.get("total"));
-        dataResponse.put("additions", stats.get("additions"));
-        dataResponse.put("deletions", stats.get("deletions"));
-        return dataResponse;
+                new ParameterizedTypeReference<Map<String, Object>>() {}
+        );
+        Map<String, Object> response = pullsResponse.getBody(); // <Map>
+        Map<String, Object> commitData = (Map<String, Object>) response.get("commit"); // <Map>
+        Map<String, Object> stats = (Map<String, Object>) response.get("stats");
+        Map<String, Object> author = (Map<String, Object>) commitData.get("author"); // <Map>
+        String authorName = (String) author.get("name");
+        String date = (String) author.get("date");
+        String message = (String) commitData.get("message");
+        Number additions = (Number) stats.get("additions");
+        Number deletions = (Number) stats.get("deletions");
+        Long linesAdded = additions.longValue();
+        Long linesRemoved = deletions.longValue();
+        GitContribution gitContribution = gitContributionRepository.findByCommitHashAndProjectId(shaCommit, projectId);
+        if (gitContribution == null) {
+            gitContribution = new GitContribution();
+            gitContribution.setProjectId(projectId);
+        }
+        gitContribution.setCommitHash(shaCommit);
+        gitContribution.setNameGithub(authorName);
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(date);
+        gitContribution.setCommitDate(zonedDateTime.toLocalDate());
+        gitContribution.setLinesAdded(linesAdded);
+        gitContribution.setLinesRemoved(linesRemoved);
+        gitContribution.setMessage(message);
+        gitContribution.setGithubLink(githubLink);
+        return gitContribution;
     }
 
-    private List<String> getListShaCommit(String githubLink, HttpEntity<String> entity, RestTemplate restTemplate, String author) {
-        String pullsUrl = githubLink + "/commits?author=" + author;
+
+
+    private List<String> getListShaCommit(String githubLink, HttpEntity<String> entity, RestTemplate restTemplate, Set<String> existingShaCommit) {
+        String pullsUrl = String.format("https://api.github.com/repos/%s/commits", githubLink);
         ResponseEntity<List<Map<String, Object>>> pullsResponse = restTemplate.exchange(
                 pullsUrl,
                 HttpMethod.GET,
                 entity,
                 new ParameterizedTypeReference<List<Map<String, Object>>>() {}
         );
-        return pullsResponse.getBody().stream().map(
-                response -> {
-                    String sha = (String) response.get("sha");
-                    return sha;
-                }
-        ).toList();
+        return pullsResponse.getBody().stream()
+                .map(response -> (String) response.get("sha"))
+                .toList();
     }
-
-
 }
